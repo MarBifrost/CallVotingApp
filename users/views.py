5fe75d0ab59435ad3598913c14882da1cd53c7ab
MarBifrost <128312989+MarBifrost@users.noreply.github.com>
2025-02-04 15:04:25 +0400

    add authorization and authentication

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from .forms import LoginForm
+
 
 # Create your views here.
+class CustomLoginView(LoginView):
+    template_name = 'signin.html'
+    form_class = LoginForm
+    redirect_authenticated_user = True
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password.')
+        return super().form_invalid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('calls:home')
+
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'signin.html'
+
+    # def dispatch(self, request, *args, **kwargs):
+    #     messages.success(request, "You have been logged out successfully.")
+    #     return super().dispatch(request, *args, **kwargs)
